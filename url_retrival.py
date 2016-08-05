0b0ea50d5e3b56b0050237b679a9ef52320138d5
Ajamal Khan <ajamal_k@live.com>
2016-08-05 19:06:33 +0530

    Added the ability to download form different market

--- url_retrival.py
+++ url_retrival.py
@@ -1,24 +1,29 @@
 import urllib,json,sys
-import save_wallpaper as sw
+import wallpaper as w
+
+MARKET = ['IN','US','AU','NZ','CN','GB','ZA']
 
 # Retrive url of image from JSON
 def find_image_url():
-    JSON_LINK = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8&mkt=en-IN"
-    try:
-        data = urllib.urlopen(JSON_LINK)
-        json_data = json.load(data)
-        if "images" in json_data:
-            images = json_data["images"]
-        else:
-            sys.exit('JSON error. Please try again later....')
-        print('Downloading...')
-        for i in range(len(images)):
-            url = 'http://www.bing.com' + images[i]['url']
-        sw.download_file(url)
+    for market in MARKET:
+        for n in range(0,9):
+            JSON_LINK = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n="+str(n+1)+"&mkt=en-"+market
+            try:
+                data = urllib.urlopen(JSON_LINK)
+                json_data = json.load(data)
+                if "images" in json_data:
+                    images = json_data["images"]
+                else:
+                    sys.exit('JSON error. Please try again later....')
+                print('Downloading...')
+                for i in range(len(images)):
+                    url = 'http://www.bing.com' + images[i]['url']
+                w.download_file(url)
 
-    except Exception as e:
-        print str(e)
+            except Exception as e:
+                print str(e)
 
 if __name__ == '__main__':
-    sw.make_directory()
+    w.make_list()
+    w.make_directory()
     find_image_url()
---
+++ wallpaper.py
@@ -0,0 +1,64 @@
+import os,urllib
+
+DIR = os.path.join(os.path.expanduser('~'),'Desktop/Bing Wallpaper/')
+wallpapers = []
+#Make a Directory if Bing Wallpaper does not exists
+def make_directory():
+    if os.path.exists(DIR):
+        pass
+    else:
+        print "Creating a Directory",DIR,'for wallpaper...'
+        os.mkdir(DIR)
+
+#Make list of all wallpapers
+def make_list():
+    os.chdir(DIR)
+    if os.path.isfile('list_of_wallpaper.txt'):
+        pass
+    else:
+        ls = os.listdir(DIR)
+        for i in ls:
+            if i.startswith("."):
+                continue
+            else:
+                a = i.split("_")
+                wallpapers.append(a[0])
+        f = open("list_of_wallpaper.txt",'w')
+        for w in wallpapers:
+            f.write(w+' ')
+        f.close()
+
+def appendToList(image):
+    with open('list_of_wallpaper.txt','a') as f:
+        f.write(image+' ')
+    return
+
+
+def check_list(name):
+    with open('list_of_wallpaper.txt','r') as f:
+        l = f.read().split()
+    if name in l:
+        return True
+    return False
+
+
+# Download Wallpaper
+def download_file(url):
+    file_name = url.split('/')[-1]
+    image = file_name.split("_")[0]
+
+    #Check wheather the wallpaper already in Bing Wallpaper Directory
+    if check_list(image):
+        print "Wallpaper", file_name,'already exists..'
+        return
+
+    #appending to List
+    appendToList(image)
+
+    os.chdir(DIR)
+
+    pic = urllib.urlopen(url)
+    with open(file_name,'w') as f:
+        f.write(pic.read())
+    print file_name,"downloaded..."
+    print "Check",DIR
